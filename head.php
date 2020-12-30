30682adcc769489ecb664342ffc16fdb68d8e899
Car Mar <misionessistemas@gmail.com>
2020-12-30 13:13:54 -0300

    modificado

--- head.php
+++ head.php
@@ -7,13 +7,30 @@
     ?> 
 	<!-- Bootstrap -->
 	<link href="bootstrap-4.5.3-dist/css/bootstrap.min.css" rel="stylesheet">
-  <link type="text/css" href="https://fontawesome.com/v3.2.1/assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link type="text/css" href="https://fontawesome.com/v3.2.1/assets/font-awesome/css/font-awesome.css" rel="stylesheet">
             
  
 	<style>
 		.content {
 			margin-top: 80px;
 		}
-	</style>
+
+		html,body{
+            height: 100%;
+            width: 100%;
+        }
+
+        .background-image {
+            background-image: url(imagenes/balloon_sky_clouds_flight_91317_1280x720.jpg) ;
+            display: block;
+            height: 100%;
+            width: 100%;
+            left: 0;
+            position: fixed;
+            right: 0;
+            z-index: 1;
+        }
+        
+ 	</style>
  
 </head>
