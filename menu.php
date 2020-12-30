6172f30fb048900b28169d90c0c92e22ab1bf841
Car Mar <misionessistemas@gmail.com>
2020-12-30 12:05:42 -0300

    para menu

---
+++ menu.php
@@ -0,0 +1,32 @@
+   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="index.php">Inicio</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+           <a class="nav-link" href="servicios.php">Servicios <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+           <a class="nav-link" href="alojamientos.php">Alojamientos <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+           <a class="nav-link" href="clientes.php">Clientes <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+           <a class="nav-link" href="reservas.php">Reservas <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+           <a class="nav-link" href="pagos.php">Pagos <span class="sr-only">(current)</span></a>
+          </li>
+        </ul>  
+         <!-- <form class="form-inline my-2 my-lg-0" action="login.php">
+             <input class="form-control mr-sm-2" type="text" placeholder="Usuario" name="username">
+             <input class="form-control mr-sm-2" type="text" placeholder="Contraseña" name="psw">
+             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
+         </form>-->
+    </div>
+	
+    </nav>
+ 
