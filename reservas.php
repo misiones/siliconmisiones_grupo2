e351a4b51e36df0fec3f576b8404593b5cc4521c
Car Mar <misionessistemas@gmail.com>
2020-12-30 12:06:30 -0300

    con menu pie y head

--- reservas.php
+++ reservas.php
@@ -1,68 +1,18 @@
 <?php include("conectar3.php");
+$pagina="Listar reservas";
+include("head.php");
 ?>
-<html>
-<head>
-    <meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Listar Reservas</title>
- 
-	<!-- Bootstrap -->
-	<link href="bootstrap-4.5.3-dist/css/bootstrap.min.css" rel="stylesheet">
-  <link type="text/css" href="https://fontawesome.com/v3.2.1/assets/font-awesome/css/font-awesome.css" rel="stylesheet">
-            
- 
-	<style>
-		.content {
-			margin-top: 80px;
-		}
-	</style>
- 
-</head>
 <body>
-
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="index.php">Inicio</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-           <a class="nav-link" href="servicios.php">Servicios <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item active">
-           <a class="nav-link" href="alojamientos.php">Alojamientos <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item active">
-           <a class="nav-link" href="clientes.php">Clientes <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item active">
-           <a class="nav-link" href="reservas.php">Reservas <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item active">
-           <a class="nav-link" href="pagos.php">Pagos <span class="sr-only">(current)</span></a>
-          </li>
-        </ul>  
-         <!-- <form class="form-inline my-2 my-lg-0" action="login.php">
-             <input class="form-control mr-sm-2" type="text" placeholder="Usuario" name="username">
-             <input class="form-control mr-sm-2" type="text" placeholder="Contraseña" name="psw">
-             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
-         </form>-->
-    </div>
-	
-    </nav>
+<?php
+include("menu.php");
+?>
   
-  
-   
 	<div class="container">
 		<div class="content">
        <div class="panel panel_default">      
 
             <div class="pull-right">
                 <a href="addreservas.php" class="btn btn-sm btn-dark">AGREGAR RESERVAS</a>
-                <!--<button type="button" class="btn btn-warning"><a href="add.php">AGREGAR SERVICIO</a></button>-->
             </div>
            <?php
                if(isset($_GET['action']) == 'delete'){
@@ -230,10 +180,6 @@ $sentencia = $mysqli ->prepare("SELECT reservas.*,clientes.nombreyap, usuario.no
         </nav>
     </div>
     </div>
-        <center>
-	<p>&copy; Grupo 2 - Programador Web Full Stack - Silicon Misiones <?php echo date("Y");?></p
-		</center>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script src="bootstrap-4.5.3-dist/js/bootstrap.min.js"></script>
-</body>
-</html>
+       <?php
+include("pie.php");
+?>
