9cd9d5ab626eba8664f0ced766b04d0b610b9d51
Car Mar <misionessistemas@gmail.com>
2020-12-30 12:05:06 -0300

    para cabecera

---
+++ head.php
@@ -0,0 +1,19 @@
+<html  dir="ltr" lang="es" xml:lang="es">
+<head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php echo "<title>".$pagina."</title>";
+    ?> 
+	<!-- Bootstrap -->
+	<link href="bootstrap-4.5.3-dist/css/bootstrap.min.css" rel="stylesheet">
+  <link type="text/css" href="https://fontawesome.com/v3.2.1/assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+            
+ 
+	<style>
+		.content {
+			margin-top: 80px;
+		}
+	</style>
+ 
+</head>
