afe1ac54bfa0d9f8f4328854162af1d479216df6
Car Mar <55630856+misiones@users.noreply.github.com>
2021-01-02 10:36:32 -0300

    Delete delete.php

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "conectar3.php";
-if(isset($_GET['idclientes'])){
-    $idclientes = $_GET['idclientes'];
-    $query = "DELETE FROM clientes WHERE idclientes = $idclientes";
-    $resultado = mysqli_query($mysqli, $query);
-    header('Location: clientes.php');
-}
-?>
