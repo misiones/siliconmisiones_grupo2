077a60244b1cef75e5210b06732bb99414643ba4
Car Mar <misionessistemas@gmail.com>
2021-01-02 10:17:08 -0300

    arreglado

--- addclientes.php
+++ addclientes.php
@@ -7,28 +7,27 @@ include("menu.php");
   <div class="container">   
                 <?php
              if(isset($_POST['add'])){
-               // $idclientes     = mysqli_real_escape_string($mysqli,(strip_tags($_POST["idclientes"],ENT_QUOTES))); 
                 $nombreyap      = mysqli_real_escape_string($mysqli,(strip_tags($_POST["nombreyap"],ENT_QUOTES))); 
                 $dni            = mysqli_real_escape_string($mysqli,(strip_tags($_POST["dni"],ENT_QUOTES))); 
                 $telefono       = mysqli_real_escape_string($mysqli,(strip_tags($_POST["telefono"],ENT_QUOTES))); 
                 $email          = mysqli_real_escape_string($mysqli,(strip_tags($_POST["email"],ENT_QUOTES))); 
                 $direccion      = mysqli_real_escape_string($mysqli,(strip_tags($_POST["direccion"],ENT_QUOTES))); 
-
- 
-                $ser = mysqli_query($mysqli, "SELECT * FROM clientes WHERE idclientes='$idclientes'");
-                if(mysqli_num_rows($ser) == 0){
-                       $insert = mysqli_query($mysqli, "INSERT INTO clientes(idclientes, nombreyap, dni, telefono, email, direccion)
-                              VALUES(NULL,'$nombreyap','$dni','$telefono,'$email','$direccion')") or die(mysqli_error());
-                        if($insert){
-                          echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Los datos han sido guardados con éxito.</div>';
-                        }else{
-                          echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Error. No se pudo guardar los datos !</div>';
-                        }
-           
+                $cek = mysqli_query($mysqli, "SELECT * FROM clientes WHERE idclientes='$idclientes'");
+                if(mysqli_num_rows($cek) == 0){
+                $insert = mysqli_query($mysqli, "INSERT INTO clientes(nombreyap, dni, telefono, email, direccion)
+                VALUES('$nombreyap', '$dni', '$telefono', '$email', '$direccion')") or die(mysqli_error());
+                if($insert){
+                echo '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Bien hecho! Los datos han sido guardados con éxito.</div>';
+               
                 }else{
-                echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Error. Ese código existe!</div>';
+                echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Error. No se pudo guardar los datos !</div>';
                 }
-            }
+                }else{
+                echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Error. código exite!</div>';
+                }
+                  
+                }
+ 
             ?>
             <div class="row justify-content-center">
               <div class="col-6 p-3 bg-white shadow-lg rounded">
@@ -37,23 +36,23 @@ include("menu.php");
                   <hr>
                  <div class="form-group">
                     <label for="validationServer02">Nombre y Apellido</label> 
-                     <input type="text" class="form-control" id="nombreyap" placeholder="Nombre y Apellido" required>
+                     <input type="text" name="nombreyap" class="form-control" id="nombreyap" placeholder="Nombre y Apellido" required>
                  </div>
                 <div class="form-group">
                     <label for="validationServer03">DNI</label>
-                    <input type="text" class="form-control" id="dni" placeholder="DNI"  required>
+                    <input type="text" name="dni" class="form-control" id="dni" placeholder="DNI"  required>
                 </div>
                  <div class="form-group">
                     <label for="validationServer04">Email</label>
-                    <input  id="email" type="email" class="form-control" placeholder="ejempo@gmail.com" required>
+                    <input  id="email" name="email" type="email" class="form-control" placeholder="ejempo@gmail.com" required>
                 </div>
                 <div class="form-group">
                     <label for="validationServer05">Teléfono</label>
-                    <input type="text" class="form-control" id="telefono" placeholder="Teléfono" required>
+                    <input type="text" name="telefono" class="form-control" id="telefono" placeholder="Teléfono" required>
                 </div>
                 <div class="form-group">
                     <label for="validationServer06">Dirección</label>
-                    <input type="text" class="form-control" id="direccion" placeholder="Dirección" required>
+                    <input type="text" name="direccion" class="form-control" id="direccion" placeholder="Dirección" required>
                 </div>
                <div class="form-group">
                    <label class="col-sm-3 control-label">&nbsp;</label>
