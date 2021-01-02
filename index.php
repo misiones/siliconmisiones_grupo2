977abb51a76d81319d0ede238440cd64b470ec59
Car Mar <misionessistemas@gmail.com>
2021-01-01 23:45:42 -0300

    arreglado

--- index.php
+++ index.php
@@ -3,22 +3,15 @@ $pagina="Sistema de reservas de alojamiento";
 include("head.php");
 include("menu.php");
 ?>
-  
-
 <!---SE AGREGA ESTE DIV PARA EL FONDO---->
  <div class="background-image"></div>
  <!--------SE AGREGA LA CLASE CONTENT-------->
- <div class="container-fluid h-100  content">
-        <div class="row h-100 justify-content-center align-items-center">
-            <div class="card col-8">
-                <div class="card-body w-100 col">
-                    <div class="row">
-                        <div class="col-6 ">
-                        </div>
-
-                    </div>
-
-                </div>
+<div class="container">
+       <div class="content">
+           <div class="panel panel_default">  
+             
+                 <h2>Sistema de reservas de Alojamiento</h2>
+             
             </div>
        </div>
 
