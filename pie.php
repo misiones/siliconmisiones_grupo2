75a50874d191193aef789f2b5771a7c7191f92db
Car Mar <misionessistemas@gmail.com>
2021-01-02 10:33:21 -0300

    arreglado

--- pie.php
+++ pie.php
@@ -1,5 +1,5 @@
       <center>
-	<p>&copy; Grupo 2 - Programador Web Full Stack - Silicon Misiones <?php echo date("Y");?></p
+	<p>&copy; Grupo 2 - Programador Web Full Stack - Silicon Misiones <?php echo date("Y");?></p>
 		</center>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
